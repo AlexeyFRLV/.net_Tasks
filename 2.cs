64935193b3c3c2a45cb9a424200ff45ccc73595b
Alexey Frolov <foritlearning@gmail.com>
2020-10-15 22:22:54 +0300

    task №2

---
+++ 2.cs
@@ -0,0 +1,28 @@
+﻿/*Написать программу, которая считывает символы с клавиатуры, пока не будет введена точка.
+ *Программа должна сосчитать количество введенных пользователем пробелов. */
+
+using System;
+
+namespace Lesson_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while(true)
+            {
+                Console.Clear();
+                Console.Write("Enter your character: ");
+                char symbol = Console.ReadKey().KeyChar;
+                if (symbol == ' ')
+                    count++;
+                if (symbol == '.')
+                    break;
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"Number of spaces: {count}");
+        }
+    }
+}
